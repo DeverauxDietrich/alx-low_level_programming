2752981f1327a768b01cacb130cc916fe40319b8
DeverauxDietrich <devrodeandietrich@gmail.com>
2022-09-08 12:57:01 -0700

    puts update 3 or 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* main - Entry point. Desription: prints Programming sentence */
+/**
+ * main - Entry point
+ * function_name - prints text to standard output, single line
+ * Description: prints using puts instead of printf
+ * Return: return zero means success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
