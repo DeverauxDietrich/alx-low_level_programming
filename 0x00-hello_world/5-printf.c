32a0257693ab149e28c7f3d16a5af7f3f1fef1ed
DeverauxDietrich <devrodeandietrich@gmail.com>
2022-09-08 13:07:45 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print to standard output using printf
+ *
+ * Return: always zero for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
