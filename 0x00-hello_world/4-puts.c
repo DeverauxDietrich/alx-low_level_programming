99535a7e65082f77826609cad6fb12303e06aed4
DeverauxDietrich <devrodeandietrich@gmail.com>
2022-09-08 06:50:02 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Desription: prints Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ * 
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
